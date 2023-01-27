9e69fb34bb67ed2b83e62ef277be62f8047abd68
natussshka <112900249+natussshka@users.noreply.github.com>
2023-01-27 19:07:56 +0100

    added the github repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Natalia Kubasi and is{" "}
+        <a
+          href="https://github.com/natussshka/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
