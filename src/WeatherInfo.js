e92899a67b3a0adf743135a9dc190391c2b71309
natussshka <112900249+natussshka@users.noreply.github.com>
2023-01-29 22:07:21 +0100

    search function

---
+++ src/WeatherInfo.js
@@ -0,0 +1,35 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo() {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img src={weatherData.iconUrl} alt={props.data.description} />
+            <div>
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind}km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
