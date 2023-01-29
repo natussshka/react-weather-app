686d25268c203b4fda0ff34e025be475b9ff7139
natussshka <112900249+natussshka@users.noreply.github.com>
2023-01-29 17:07:44 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Natalia Kubasi and is{" "}
-        <a
-          href="https://github.com/natussshka/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Natalia Kubasi and is{" "}
+          <a
+            href="https://github.com/natussshka/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
